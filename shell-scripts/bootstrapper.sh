916749a192c8d8d98d7c78b26c8a881f0834418a
Daniel <daniel.kortesmaa@edu.turkuamk.fi>
2025-04-30 17:19:26 +0300

    updated bootstrapper

--- shell-scripts/bootstrapper.sh
+++ shell-scripts/bootstrapper.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e 
 
-USER_NAME=${SUDO_USER:$(whoami)}
+IS_ROOT=0
+if [[ "$EUID" -eq 0 ]]; then
+  IS_ROOT=1
+fi
+
+if [[ $IS_ROOT -eq 1 && -n "$SUDO_USER" ]]; then
+  USER_NAME="$SUDO_USER"
+else
+  USER_NAME="$(whoami)"
+fi
+
 USER_HOME=$(eval echo "~$USER_NAME")
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -12,4 +22,10 @@ chmod +x setup-neovim.sh
 
 echo "Running user setup as $USER_NAME..."
 chmod +x setup-dotfiles.sh
-sudo -u "$USER_NAME" "$SCRIPT_DIR/setup-dotfiles.sh" "$USER_HOME"
+
+
+if [[ $IS_ROOT -eq 1 && "$USER_NAME" != "root" ]]; then
+  sudo -u "$USER_NAME" "$SCRIPT_DIR/setup-dotfiles.sh" "$USER_HOME"
+else
+  "$SCRIPT_DIR/setup-dotfiles.sh" "$USER_HOME"
+fi
